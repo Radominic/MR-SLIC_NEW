666592661d2e19ae0d0cc0cfc0003bbb21ee950c
Radominic <wjdrmf314@gmail.com>
2020-03-02 18:23:24 +0900

    gcp version

--- overlap/MR_SLIC_overlap.py
+++ overlap/MR_SLIC_overlap.py
@@ -9,67 +9,39 @@ from scipy import ndimage as ndi
 from skimage.segmentation import slic
 from PIL import Image
 from skimage.util import img_as_float
-from skimage.segmentation._slic_master import _slic_cythonM
 import numpy as np
+
+
+#import sys
+#sys.path.append('/home/gangmin/anaconda3/lib/python3.6/site-packages/skimage')
+from skimage.segmentation._slic_master import _slic_cythonM
+from skimage.segmentation._slic import _enforce_label_connectivity_cython
 from skimage.color import rgb2lab
-sc = SparkContext()
+
+
+
+
+#sc = SparkContext()
 
 #number of partition
-partition = 4
-
+partition = 3
 #resource file name
 k=['house_2_1','house_2_2','house_2_3']
+#파티션된 이미지 조각의 픽셀 인덱스
 listi = [0,180,360]
-
-# loop over the number of segments
+#목표 세그먼트
+OriginSegments = 500
+# 이미지 조각당 세그먼트 수 
 numSegments = 250
-
-
-#원본 이미지 메타데이터 얻기위한 전처리 
-image = Image.open( '/home/wjdrmf314/MR_SLIC_NEW/resource/house.jpg')
-image = img_as_float(image)
-is_2d = False
-if image.ndim == 2:
-    # 2D grayscale image
-    image = image[np.newaxis, ..., np.newaxis]
-    is_2d = True
-elif image.ndim == 3 and True:
-    # Make 2D multichannel image 3D with depth = 1
-    image = image[np.newaxis, ...]
-    is_2d = True
-elif image.ndim == 3 and not True:
-    # Add channel as single last dimension
-    image = image[..., np.newaxis]
-
-if True:
-    spacing = np.ones(3)
-elif isinstance(spacing, (list, tuple)):
-    spacing = np.array(spacing, dtype=np.double)
-sigma = 5
-if not isinstance(sigma, coll.Iterable):
-    sigma = np.array([sigma, sigma, sigma], dtype=np.double)
-    sigma /= spacing.astype(np.double)
-elif isinstance(sigma, (list, tuple)):
-    sigma = np.array(sigma, dtype=np.double)
-if (sigma > 0).any():
-    # add zero smoothing for multichannel dimension
-    sigma = list(sigma) + [0]
-    image = ndi.gaussian_filter(image, sigma)
-
-convert2lab = None
-if True and (convert2lab or convert2lab is None):
-    if image.shape[-1] != 3 and convert2lab:
-        raise ValueError("Lab colorspace conversion requires a RGB image.")
-    elif image.shape[-1] == 3:
-        image = rgb2lab(image)
-
-depth, height, width = image.shape[:3]
-
-
-
-
-
-
+#원본 이미지 크기
+(img_h,img_w) = 720,1280
+depth = 1
+height = img_h
+width = img_w
+#원본 이미지 메타데이터(결과 출력용)
+#images = Image.open('/home/gangmin/MR-SLIC_NEW/resource/house.jpg')
+#images = img_as_float(images)
+#map 함수
 def Map(k):
     #time check
     start_time = time.time()
@@ -77,7 +49,7 @@ def Map(k):
     
     #read imagez
     #submit image with job 
-    img = Image.open( k +'.jpg')
+    img = Image.open('/home/gangmin/MR-SLIC_NEW/resource/'+ k +'.jpg')
     
     image = img_as_float(img)
     #슬레이브 노드로 작업 분배
@@ -95,36 +67,29 @@ stime = time.time()
 datas = sc.parallelize(k,partition).map(Map).collect()
 ftime = time.time()
 
-#datas 어레이에서 distance, nearest segments 뽑아내기
-
-
-#원본 이미지 메타데이터
+#원본 이미지 메타데이터로 dimension 전송
 dimension = [depth,height,width]
-dimension = np.array(dimension)
+dimension = np.ascontiguousarray(dimension)
 
 #segments, distances 리스트 만들기
 seglist = []
 distlist = []
 indlist = []
 for i in datas:
-    seglist += [i[0]]
+    seglist += [i[0][np.newaxis,:]]
     distlist += [i[1]]
-    indlist += [i[4]]
+    indlist += [int(i[4])]
 
-result = _slic_cythonM(distlist,seglist,indlist,dimension,listi)
+result = _slic_cythonM(np.ascontiguousarray(distlist),np.ascontiguousarray(seglist),np.ascontiguousarray(indlist),dimension,np.ascontiguousarray(listi))
 
-#cython 파일을 실행한다.
-
-#전체 이미지 크기의 label을 생성한다.(메타데이터)
-
-#전달받은 nearest_segments 데이터와 distance 데이터를 이용해 새로운 label을 갱신한다.
-
-#마스터로 리턴받는다.
-
-
-
-
-
+#label 결속 처리
+if 1:
+    segment_size = depth * height * width / OriginSegments
+    min_size = int(0.5 * segment_size)
+    max_size = int(3 * segment_size)
+    labels = _enforce_label_connectivity_cython(result,min_size,max_size)
+fftime = time.time()
+    
 #time check
 import pandas as pd
 data = pd.DataFrame(columns=['node','time'])
@@ -142,9 +107,9 @@ for i in datas:
     node+=1
 
 #network time
-for i in [0,2,4,6]:
+for i in [0,2,4]:
     print('network_ in_time : '+str(data['time'][i]-stime))
-for i in [1,3,5,7]:
+for i in [1,3,5]:
     print('network_ out_time : '+str(ftime-data['time'][i]))
     
     
@@ -157,7 +122,30 @@ print(data)
 
 print(datat)
 
+#reduce time
+print('master_time : '+str(fftime-ftime))
+
 #whole time
-print('whole_time : '+str(ftime-stime))
+print('whole_time : '+str(fftime-stime))
 
 
+
+'''
+import matplotlib.pyplot as plt
+#save result
+fig = plt.figure("Superpixels -- %d segments" % (numSegments),dpi = 96,figsize = (img_h/96,img_w/96))
+ax = fig.add_subplot(1, 1, 1)
+
+
+ax.imshow(mark_boundaries(images, labels[0]))
+plt.xticks([]), plt.yticks([])
+plt.tight_layout()
+plt.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1, hspace = 0, wspace = 0)
+    
+#plt.savefig('/home/wjdrmf314/resource' + '/raptors%d.png'%numSegments,bbox_inces='tight', 
+#            pad_inches=0)
+plt.axis("off")
+
+# show the plots
+plt.show()
+'''
