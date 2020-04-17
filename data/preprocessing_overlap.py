95799a77269375e2789045c7133e91b470f07af0
Radominic <wjdrmf314@gmail.com>
2020-04-17 17:58:05 +0900

    overlap code update

--- data/preprocessing_overlap.py
+++ data/preprocessing_overlap.py
@@ -11,9 +11,9 @@ Created on Wed Apr  1 14:22:53 2020
 
 @author: Gangmin
 """
-
 # list for the number of image split
-splits = [8]
+overlaps = [0,20,40,60,80]
+
 
 
 import os
@@ -24,8 +24,10 @@ file_list_img = [file for file in file_list if file.endswith(".jpg")]
 #save to csv file.
 import pandas as pd
 from PIL import Image
-
-for split in splits:
+temp = 0
+for overlap in overlaps:
+    percent = int(overlap/2)
+    split = 8
     for img_name in file_list_img:
         # load the image and apply SLIC and extract (approximately)
         # the supplied number of segments
@@ -39,27 +41,24 @@ for split in splits:
             if remain is not 0:
                 split_list[0] = split_list[0]+1
                 remain -=1
-        print(split_list)
-        for i in range(1,split):
-            index=0
-            index_start = 0
+        index_start = 0
+        
+        
+        for i in range(0,split):
+            extension = int(split_list[i]*percent/100)
+            #i만큼 붙여서 j번 반복
+            index_end =0
+            for k in range(0,i+1):
+                index_end += split_list[k]
+            area = (0,max(0,index_start-extension),size[0],min(index_end+extension,size[1]))
             
-            for j in range(9-i):
-                #i만큼 붙여서 j번 반복
-                index_end =0
-                for k in range(i):
-                    index_end += split_list[index+k]
-                area = (0,index_start,size[0],index_end+index_start)
-                
-                
-                #cropped_img = img.crop(area)
-                #cropped_img.save("C:/Users/Gangmin/Desktop/SLIC/논문작업/sourcecode/overlap_image/"+str(i)+"/"+img_name[:-4]+"_"+str(index)+".jpg")
-                
-                
-                index_start += split_list[index]
-                index+=1
-        break
-    break
+            #print(img_name)
+            #print(area)
+            cropped_img = img.crop(area)
+            cropped_img.save("C:/Users/Gangmin/Desktop/SLIC/논문작업/sourcecode/overlap_image/"+str(overlap)+"/"+img_name[:-4]+'_'+str(i)+".jpg")
+            index_start += split_list[i]
+        
+        
         
     
     
